68675481e8d9d0dce2578350e27e0e84cd93bd11
ibermaticai3b <118179797+ibermaticai3b@users.noreply.github.com>
2023-01-05 12:15:42 +0100

    DQR database constraints fix

--- ai-service-PEAA/DQR/tables.sql
+++ ai-service-PEAA/DQR/tables.sql
@@ -1,31 +1,32 @@
 USE `results_db`;
 DROP TABLE IF EXISTS `DQR_report`;
 CREATE TABLE `DQR_report` (
-	`id` INT NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `ts_start_run` TIMESTAMP NOT NULL,
-	`ts_end_run` TIMESTAMP NULL,
-	`summary_report` BLOB,
-   PRIMARY KEY (`id`)
+    `ts_end_run` TIMESTAMP NULL,
+    `summary_report` MEDIUMBLOB,
+    PRIMARY KEY (`id`)
 );
 
 USE `results_db`;
 DROP TABLE IF EXISTS `DQR_report_variable`;
 CREATE TABLE `DQR_report_variable` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`dqr_report_id` INT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `dqr_report_id` INT NOT NULL,
     `variable` NVARCHAR(1024) NULL,
-	`distinct` BIGINT NULL,
-    `distinct_percentage` DECIMAL(3,2) NULL,
-	`missing` BIGINT NULL,
-    `missing_percentage` DECIMAL(3,2) NULL,
+    `distinct` BIGINT NULL,
+    `distinct_percentage` DECIMAL NULL,
+    `missing` BIGINT NULL,
+    `missing_percentage` DECIMAL NULL,
     `infinite` BIGINT NULL,
-    `infinite_percentage` DECIMAL(3,2) NULL,
+    `infinite_percentage` DECIMAL NULL,
     `zeros` BIGINT NULL,
-    `zeros_percentage` DECIMAL(3,2) NULL,
+    `zeros_percentage` DECIMAL NULL,
     `negative` BIGINT NULL,
-    `negative_percentage` DECIMAL(3,2) NULL,
-	`min` DOUBLE NULL,
-	`max` DOUBLE NULL,
+    `negative_percentage` DECIMAL NULL,
+    `mean` DOUBLE NULL,
+    `min` DOUBLE NULL,
+    `max` DOUBLE NULL,
     `std` DOUBLE NULL,
     `variance` DOUBLE NULL,
     `iqr` DOUBLE NULL,
@@ -35,5 +36,7 @@ CREATE TABLE `DQR_report_variable` (
     `p50` DOUBLE NULL,
     `p75` DOUBLE NULL,
     `p95` DOUBLE NULL,
-   PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    KEY `dqr_report_id` (`dqr_report_id`),
+    CONSTRAINT `DQR_report_variable_ibfk_1` FOREIGN KEY (`dqr_report_id`) REFERENCES `DQR_report` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
