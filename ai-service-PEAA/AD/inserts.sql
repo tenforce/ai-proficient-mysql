2907a5089e5b3819a5a7abd9fdfc166810b81518
sanumnisar <sanumnisar@hotmail.com>
2023-01-09 13:31:31 +0100

    Fixing table typo in insert.sql

--- ai-service-PEAA/AD/inserts.sql
+++ ai-service-PEAA/AD/inserts.sql
@@ -6,5 +6,5 @@ SET @ad_report_id = LAST_INSERT_ID();
 INSERT INTO `AD_report_quasi_constant_variables` (`ad_report_id`, `variable`)
 VALUES (@ad_report_id, 'variable 1');
 
-INSERT INTO `AD_report_variable_importance` (`ad_report_id`, `method`, `model_accuracy_score_with_criterion_gini_index`, `training_set_accuracy_score`, `training_set_score`, `test_set_score`, `precision`, `recall`, `f1_score`, `general_score`)
+INSERT INTO `AD_report_decision_tree_method` (`ad_report_id`, `method`, `model_accuracy_score_with_criterion_gini_index`, `training_set_accuracy_score`, `training_set_score`, `test_set_score`, `precision`, `recall`, `f1_score`, `general_score`)
 VALUES (@ad_report_id, 'decision tree', 0.8, 0.9, 0.85, 0.75, 0.7, 0.8, 0.75, 0.77);
